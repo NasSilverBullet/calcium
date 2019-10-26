fa67e2a9f47dd87b999b704f704310a0b687f77a
nas <nas.silver.bullet@gmail.com>
2019-10-26 22:09:37 +0900

    add test for tasks

--- pkg/calcium/tasks_test.go
+++ pkg/calcium/tasks_test.go
@@ -1,1 +1,25 @@
 package calcium_test
+
+import (
+	"testing"
+
+	"github.com/NasSilverBullet/calcium/pkg/calcium"
+)
+
+func TestTasksUsage(t *testing.T) {
+	tests := []struct {
+		name  string
+		tasks calcium.Tasks
+		want  string
+	}{
+		{"HasTasks", calcium.Tasks{&calcium.Task{Use: "test"}, &calcium.Task{Use: "test2", Flags: calcium.Flags{&calcium.Flag{}}}}, "Usage:\n  ca run test\n  ca run test2 [Flags]"},
+		{"NoTasks", calcium.Tasks{}, "Usage:\n  Tasks not found"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.tasks.Usage(); tt.want != got {
+				t.Errorf("tasks.Usage() => got %s, but want %s", got, tt.want)
+			}
+		})
+	}
+}
